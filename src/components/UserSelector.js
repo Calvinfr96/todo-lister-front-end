16f80ec8b80bbd573f9bde6cc1405f64ce93c468
calvin <calvin.francis96@outlook.com>
2021-08-14 12:43:40 -0400

    Fix bug in user dropdown menu

--- src/components/UserSelector.js
+++ src/components/UserSelector.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function UserSelector({users, currentUser, setCurrentUser, setHeaderText}) {
     const options = users.map(user => <option key={user.id} value={`${user.id}-${user.name}`} >{user.name}</option>)
 
+    const [optionValue, setOptionValue] = useState("none")
+
     function handleSelection(event) {
         const [userID, userName] = event.target.value.split('-')
+        setOptionValue(event.target.value)
         setCurrentUser(userID)
         setHeaderText(`${userName}'s To Do's`)
     }
@@ -12,7 +15,7 @@ function UserSelector({users, currentUser, setCurrentUser, setHeaderText}) {
     return (
         <div>
             <span>Select User to View Tasks: </span>
-            <select value={currentUser} onChange={handleSelection}>
+            <select value={optionValue} onChange={handleSelection}>
                 <option value="none" disabled>Select User</option>
                 {options}
             </select>
